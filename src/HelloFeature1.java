858db028946aa09011ece1271a84625b1b705ab9
Sajjafari74 <Sajjafari74@gmail.com>
2020-09-25 12:43:09 +0330

    Add feature2

--- src/HelloFeature1.java
+++ src/HelloFeature1.java
@@ -2,5 +2,7 @@ public class HelloFeature1 {
     public static void main(String[] args) {
         System.out.println("Hello feature1");
         System.out.println("Bye feature1");
+
+        System.out.println("Hello feature2");
     }
 }
